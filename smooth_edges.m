15dbf6e1aa2dbb7b1c3f62cc6085cf8b3c35586e
Bryce Askey <50186472+bryceaskey@users.noreply.github.com>
2019-07-19 18:27:41 -0500

    Delete smooth_edges.m

--- smooth_edges.m
+++
@@ -1,7 +0,0 @@
-%smooth edges of binary image with savitzky-golay filter method
-
-%identify edges
-image = leaves;
-boundaries = bwboundaries(leaves);
-for ii = 1:1:size(boundaries, 1)
-    boundary = 
